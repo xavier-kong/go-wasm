cbd133609f15d4d24cf0a1d4777ef92ed14d7d50
xavier-kong <kongxianchengxavier@gmail.com>
2024-03-12 22:36:09 +0800

    this was not what i had in mind

--- cmd/wasm/main.go
+++ cmd/wasm/main.go
@@ -2,6 +2,7 @@ package main
 
 import (
 	"encoding/json"
+	"errors"
 	"fmt"
 	"syscall/js"
 )
@@ -21,27 +22,38 @@ func prettyJson(input string) (string, error) {
 func jsonWrapper() js.Func {
 	jsonfunc := js.FuncOf(func(this js.Value, args []js.Value) any {
 		if len(args) != 1 {
-			return "Invalid no of arguments passed"
+			result := map[string]any{
+				"error": "Invalid no of arguments passed",
+			}
+			return result
 		}
 		jsDoc := js.Global().Get("document")
 		if !jsDoc.Truthy() {
-			return "Unable to get document object"
+			result := map[string]any{
+				"error": "Unable to get document object",
+			}
+			return result
 		}
 		jsonOuputTextArea := jsDoc.Call("getElementById", "jsonoutput")
 		if !jsonOuputTextArea.Truthy() {
-			return "Unable to get output text area"
+			result := map[string]any{
+				"error": "Unable to get output text area",
+			}
+			return result
 		}
 		inputJSON := args[0].String()
 		fmt.Printf("input %s\n", inputJSON)
 		pretty, err := prettyJson(inputJSON)
 		if err != nil {
 			errStr := fmt.Sprintf("unable to parse JSON. Error %s occurred\n", err)
-			return errStr
+			result := map[string]any{
+				"error": errStr,
+			}
+			return result
 		}
 		jsonOuputTextArea.Set("value", pretty)
 		return nil
 	})
-
 	return jsonfunc
 }
 
